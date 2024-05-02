8bf13f5d73bffca526831bfcaa0845ef2ddb1dce
YummyABH <harchilavaarsen2@gmail.com>
2024-05-03 02:14:08 +0300

    add dist

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite'
 import { fileURLToPath, URL } from 'node:url'
 import vue from '@vitejs/plugin-vue'
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  publicPath: 'vueShop',
   resolve: {
     alias: {
       '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
