b0cff0b3fcfb7d6312aea1192592e2a5cb7dc03d
YummyABH <harchilavaarsen2@gmail.com>
2024-05-03 02:17:59 +0300

    add dis

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  publicPath: 'vueShop',
+  publicPath: '/vueShop/',
   resolve: {
     alias: {
       '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
